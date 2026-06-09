b25c5017ac909b7f2350bad8a815da306ec1f926
EvoRulz <evo93453@gmail.com>
2026-06-09 20:51:56 +1000

    v295

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
-// @version 294
-var SC_VERSION = '@version 294';
+// @version 295
+var SC_VERSION = '@version 295';
 /*
  * app.js
  * Bootstrap, header wiring, export/import, undo/redo.
